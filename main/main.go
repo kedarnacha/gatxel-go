396766af2abd6ba8aa27e7caac820931ea903f55
kedarnacha <kedaranugerahnachaputra@gmail.com>
2025-02-24 14:47:44 +0700

    go setup

--- main/main.go
+++ main/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"log"
+	// "log"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"gatxel-appointment/config"
-	"gatxel-appointment/routes"
+	// "github.com/joho/godotenv"
+	// "gatxel-appointment/config"
+	// "gatxel-appointment/routes"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
-	db := config.InitDB()
+	// db := config.InitDB()
 
 	r := gin.Default()
 
-	routes.SetupRoutes(r, db)
+	// routes.SetupRoutes(r, db)
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello, World!"})
+		c.JSON(200, gin.H{"message": "Hello, Guyss!"})
 	})
 	r.Run(":8080")
 }
