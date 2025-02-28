baea0a9addf56ff11cb3f810dbf37f1c581be73a
YusronAbi <yusronabimuliabakhtiar@gmail.com>
2025-02-28 16:59:58 +0700

    add

--- models/appoinment.go
+++ models/appoinment.go
@@ -1,10 +1,11 @@
 package models
 
 import (
+	"context"
 	"time"
 )
 
-type Appointment struct {
+type Appoinment struct {
 	ID          int64     `json:"id"`
 	UserID      int64     `json:"user_id"`
 	Title       string    `json:"title"`
@@ -13,4 +14,11 @@ type Appointment struct {
 	EndTime     time.Time `json:"end_time"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
+}
+type AppoinmentRepository interface {
+	GetAllAppoinment(ctx context.Context) ([]*Appoinment, error)
+	GetAppoinmentByID(ctx context.Context, id int64) (*Appoinment, error)
+	CreateAppoinment(ctx context.Context, category *Appoinment) (*Appoinment, error)
+	UpdateAppoinmentByID(ctx context.Context, id int64, data map[string]interface{}) (*Appoinment, error)
+	DeleteAppoinmentByID(ctx context.Context, id int64) error
 }
