f9f2aad3e188ea09c55ed5492448c7012a748db4
YusronAbi <yusronabimuliabakhtiar@gmail.com>
2025-02-28 17:10:29 +0700

    add

--- models/notification.go
+++ models/notification.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"context"
 	"time"
 )
 
@@ -11,4 +12,12 @@ type Notification struct {
 	Message       string    `json:"message"`
 	CreatedAt     time.Time `json:"created_at"`
 	IsSent        bool      `json:"is_sent"`
+}
+
+type NotificationRepository interface {
+	GetAllNotification(ctx context.Context) ([]*Notification, error)
+	GetNotificationByID(ctx context.Context, id int64) (*Notification, error)
+	CreateNotification(ctx context.Context, category *Notification) (*Notification, error)
+	UpdateNotificationByID(ctx context.Context, id int64, data map[string]interface{}) (*Notification, error)
+	DeleteNotificationByID(ctx context.Context, id int64) error
 }
