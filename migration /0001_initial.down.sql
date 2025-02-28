6dd302110fd4ddc16de1790186ee08469fd05599
kedarnacha <kedaranugerahnachaputra@gmail.com>
2025-02-28 21:01:39 +0700

    fixing import

--- migration /0001_initial.down.sql
+++
@@ -1,8 +0,0 @@
--- Drop 'notifications' table if it exists
-DROP TABLE IF EXISTS notifications;
-
--- Drop 'appointments' table if it exists
-DROP TABLE IF EXISTS appointments;
-
--- Drop 'users' table if it exists
-DROP TABLE IF EXISTS users;
