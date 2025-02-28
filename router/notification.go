8b05a356f82858e9756f2f107e290093c49c6b14
YusronAbi <yusronabimuliabakhtiar@gmail.com>
2025-02-28 16:44:09 +0700

    repair

--- router/notification.go
+++ router/notification.go
@@ -1,13 +1,17 @@
 package router
 
 import (
+	"gatxel-appointment/handler"
+	"gatxel-appointment/middleware"
+	"gatxel-appointment/repository"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetupNotificationRouter(r *gin.Engine, db *gorm.DB) {
 	notificationRepository := repository.NewNotificationRepository(db)
-	productHandler := handler.NewHandlerProduct(notificationRepository)
+	productHandler := handler.NewHandlerNotification(notificationRepository)
 
 	notification := r.Group("/notification")
 	notification.Use(middleware.AuthProtected(db))
