bc422dcebafb6faa4a42da66dc8cf24645d5c420
YusronAbi <yusronabimuliabakhtiar@gmail.com>
2025-03-14 14:41:00 +0700

    repair

--- router/auth.go
+++ router/auth.go
@@ -12,8 +12,8 @@ func SetupAuthRouter(r *gin.Engine, authService *service.AuthService) {
 	authHandler := handler.NewAuthHandler(authService)
 	auth := r.Group("/auth")
 	{
-		auth.POST("/login", authHandler.Login)
 		auth.POST("/register", authHandler.Register)
+		auth.POST("/login", authHandler.Login)
 		auth.POST("/logout", authHandler.Logout)
 
 	}
